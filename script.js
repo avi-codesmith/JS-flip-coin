95d8c4fc6f8edbd36ab61f186602ec075542384d
avi-codesmith <avi.codesmith@gmail.com>
2025-03-14 12:04:19 +0530

    Implement in Styles

--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 
 const btn = document.querySelector("button");
 const coin = document.querySelector(".coin");
+const wrapper = document.querySelector(".wrapper");
 const hCounts = document.querySelector(".h-counts");
 const tCounts = document.querySelector(".t-counts");
 const again = document.querySelector("svg");
 const sound = new Audio("coin.mp3");
 
 const change = () => {
+  btn.classList.add("active");
+  setTimeout(() => {
+    btn.classList.remove("active");
+  }, 100);
   sound.play();
   const random = Math.floor(Math.random() * 2);
   coin.classList.remove("bounce");
@@ -34,8 +39,18 @@ const reset = () => {
   tCounts.textContent = 0;
 };
 
+const addClass = () => {
+  coin.classList.add("bounce");
+};
+
+const animate = () => {
+  wrapper.style.transform = "translateY(0)";
+};
+
 btn.addEventListener("click", change);
 again.addEventListener("click", reset);
+window.addEventListener("load", addClass);
+window.addEventListener("load", animate);
 document.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     change();
