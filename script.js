96325d18ce57d428cb46ccfc440536607d375e72
avi-codesmith <avi.codesmith@gmail.com>
2025-03-14 11:44:22 +0530

    Implement in code

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 const btn = document.querySelector("button");
 const coin = document.querySelector(".coin");
+const hCounts = document.querySelector(".h-counts");
+const tCounts = document.querySelector(".t-counts");
+const again = document.querySelector("svg");
 const sound = new Audio("coin.mp3");
 
 const change = () => {
@@ -13,13 +16,26 @@ const change = () => {
   setTimeout(() => {
     if (random === 0) {
       coin.textContent = "Heads";
+      if (coin.textContent === "Heads") {
+        hCounts.textContent++;
+      }
     } else {
       coin.textContent = "Tails";
+      if ((coin.textContent = "Tails")) {
+        tCounts.textContent++;
+      }
     }
   }, 300);
 };
 
+const reset = () => {
+  coin.textContent = "Heads";
+  hCounts.textContent = 0;
+  tCounts.textContent = 0;
+};
+
 btn.addEventListener("click", change);
+again.addEventListener("click", reset);
 document.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     change();
